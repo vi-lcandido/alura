d49f897110b318953f41933e21d0f132aa22a942
vi-lcandido <vitoria.lcandido@gmail.com>
2023-07-03 11:35:33 -0300

    headerConteiner

--- fullstack/alura-books/src/componentes/Header/index.js
+++ fullstack/alura-books/src/componentes/Header/index.js
@@ -1,15 +1,22 @@
-import './style.css'
-import Logo from '../Logo'
-import OpcoesHeader from '../OpcoesHeader'
-import IconesHeader from '../IconsHeader'
+import './style.css';
+import Logo from '../Logo';
+import OpcoesHeader from '../OpcoesHeader';
+import IconesHeader from '../IconsHeader';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header `
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+` 
 
 function Header() {
     return(
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo/>
             <OpcoesHeader/>
             <IconesHeader/>
-        </header>
+        </HeaderContainer>
     )
 }
 
