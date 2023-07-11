e0c34e8e190113674d53b3bf6096039bd35bea77
vi-lcandido <vitoria.lcandido@gmail.com>
2023-07-10 22:40:32 -0300

    Módulo 4 - criando componente search até video 2

---
+++ fullstack/alura-books/src/componentes/Search/index.js
@@ -0,0 +1,38 @@
+import Input from "../Input";
+import styled from "styled-components";
+
+const SearchContainer = styled.section`
+    background-image: linear-gradie (90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
+`
+
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+`
+
+const Subtitle = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
+function Search(){
+    return(
+        <SearchContainer>
+            <Title>Já sabe por onde começar?</Title>
+            <Subtitle>Encontre seu livro em nossa estante.</Subtitle>
+            <Input 
+                placeholder="Escreva sua próxima leitura"
+            />
+        </SearchContainer>
+    )
+}
+
+export default Search;
