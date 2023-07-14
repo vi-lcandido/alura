9b6b89217aad7ad30b27907edf6d63b31968d567
vi-lcandido <vitoria.lcandido@gmail.com>
2023-07-13 22:54:00 -0300

    implimentação do estado de livro procurado, busca e aparição de livro a partir do objeto

--- fullstack/alura-books/src/componentes/Search/index.js
+++ fullstack/alura-books/src/componentes/Search/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from 'react';
+import { books } from './searchData'
 
 const SearchContainer = styled.section`
     background-image: linear-gradie (90deg, #002F52 35%, #326589 165%);
@@ -24,17 +25,48 @@ const Subtitle = styled.h3`
     margin-bottom: 40px;
 `
 
+const Result = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: 20px 0px;
+// width: 40%;
+border:1px white solid;
+border-radius:10px;
+cursor:pointer;
+img{
+    width:100px;
+}
+
+&:hover{
+    background-color: rgba(255, 255, 255, .4);
+}
+
+`
+
 function Search(){
-    const [writtenInput, setWrittenInput] = useState("");
+    const [searchedBooks, setSearchedBooks] = useState([]);
 
     return(
         <SearchContainer>
             <Title>Já sabe por onde começar?</Title>
             <Subtitle>Encontre seu livro em nossa estante.</Subtitle>
             <Input 
+                type="text"
                 placeholder="Escreva sua próxima leitura"
-                onBlur={event => setWrittenInput(event.target.value)}
+                onBlur={event => {
+                    const searchText = event.target.value
+                    const searchResult = books.filter(book => book.name.includes(searchText))
+                    setSearchedBooks(searchResult)
+                }}
             />
+              { searchedBooks.map( book => (
+                <Result>
+                    <Subtitle>{book.name}</Subtitle>
+                    <img src={book.src}/>
+                </Result>
+            ) ) }
         </SearchContainer>
     )
 }
