e6371004d14bd61fbeadb4c9240beb970f1b0040
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-06 20:23:41 -0300

    modulo 5: video 4 - props

--- fullstack/alura-books/src/componentes/LastReleases/index.js
+++ fullstack/alura-books/src/componentes/LastReleases/index.js
@@ -1,6 +1,8 @@
 import { books } from "./dataLastReleases"
 import styled from 'styled-components';
 import { Title } from "../Title";
+import RecomendedCard from "../RecomendedCard";
+import imgBook from "../../imagens/livro2.png"
 
 const LastReleasesContainer = styled.section `
     background-color: #EBECEE;
@@ -23,12 +25,20 @@ function LastReleases() {
         <Title 
             color="#EB9B00" 
             fontSize='36px' 
-        >Últimos Lançamentos</Title>
+        >
+            Últimos Lançamentos
+        </Title>
         <NewBooksContainer>
         {books.map(book => (
             <img src={book.src}/>
         ))}
         </NewBooksContainer>
+        <RecomendedCard
+            title="Talvez você se interesse por"
+            subtitle="Angular 11"
+            description="Construindo uma aplicação com a plataforma google"
+            img={imgBook}
+        />
     </LastReleasesContainer>
     )
 }
