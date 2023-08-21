8cf96c6d2ce25e2a8daedf39521e084ed2f2a08e
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-21 20:51:32 -0300

    feat: cria rotas de diferentes tipos no banco

---
+++ fullstack/alura-books-server/routes/livro.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const { getBooks, getBook } = require("../controllers/livro");
+
+const router = Router();
+
+router.get('/', getBooks);
+
+router.get('/:id', getBook);
+
+
+router.post('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo POST")
+});
+
+router.patch('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PATCH")
+});
+
+router.delete('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo DELETE")
+});
+
+module.exports = router;
